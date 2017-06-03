1cf10683a19e73a5a77db177342769aaa998e41b
Bas Tuijnman <bas.tuijnman@24i.com>
2017-06-03 13:09:14 +0200

    Added base page

--- src/js/ui/app/index.js
+++ src/js/ui/app/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-export default class App extends React.Component {
+const style = {
+    backgroundImage: 'linear-gradient(-180deg, rgba(255,255,255,0.50) 0%, rgba(0,0,0,0.50) 100%), radial-gradient(50% 136%, #3B8686 0%, #0B486B 100%)',
+    width: '100%',
+    height: '100%'
+};
+
+class App extends React.Component {
 
     render () {
+
         return (
-            <div>Test</div>
+            <div style={style}>
+
+            </div>
         )
+
     }
 
 }
+
+// Render the actual application
+ReactDOM.render(<App />, document.getElementById('app'));
--- src/js/ui/index.js
+++ src/js/ui/index.js
@@ -1,9 +1,11 @@
-let app = require('express')();
+let express = require('express');
+let app = express();
 let http = require('http').Server(app);
 
-app.get('/', (request, response) => {
-    response.send('<h1>CarPI UI</h1>');
-});
+// app.get('/', (request, response) => {
+//     response.send('<h1>CarPI UI</h1>');
+// });
+app.use(express.static('assets'));
 
 http.listen(3000, function () {
     console.log('Server started listening');
