b7d6db39a75628bbef6320a92dd9970ec1f1ee2d
Bas Tuijnman <bas@t-fox.nl>
2017-06-03 12:59:11 +0000

    Use absolute directory path for static file serving

--- src/js/ui/index.js
+++ src/js/ui/index.js
@@ -1,11 +1,12 @@
 let express = require('express');
 let app = express();
 let http = require('http').Server(app);
+let path = require('path');
 
 // app.get('/', (request, response) => {
 //     response.send('<h1>CarPI UI</h1>');
 // });
-app.use(express.static('assets'));
+app.use('/', express.static(path.join(__dirname, 'assets')));
 
 http.listen(3000, function () {
     console.log('Server started listening');
