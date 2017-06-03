88f120f284b8f6d58eeb4e672a77343d01e5ec8e
Bas Tuijnman <bas.tuijnman@24i.com>
2017-06-03 12:49:05 +0200

    Added application index file

--- src/js/ui/app/index.js
+++ src/js/ui/app/index.js
@@ -1,1 +1,11 @@
-console.log('Woop');
+import React from 'react';
+
+export default class App extends React.Component {
+
+    render () {
+        return (
+            <div>Test</div>
+        )
+    }
+
+}
