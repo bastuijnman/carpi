53334503a4c1e607faaf3b39632038a4f8b05e48
Bas Tuijnman <bas@t-fox.nl>
2017-06-04 12:37:04 +0000

    Make sure we're able to connect to the DBus interface once a device is connected

--- src/js/audio-controls/index.js
+++ src/js/audio-controls/index.js
@@ -1,19 +1,56 @@
 let dbus = require('dbus');
 let bus = dbus.getBus('system');
+let udev = require('udev');
+let exec = require('child_process').exec;
+let monitor = udev.monitor();
 
-bus.getInterface('org.bluez', '/org/bluez/hci0/dev_6C_72_E7_08_7A_FE/player0', 'org.bluez.MediaPlayer1', (err, interface) => {
-    if (err) {
-        console.log('MAJOR ERROR', err);
-        return;
-    }
-
-    setInterval(function () {
-        interface.getProperty('Track', function (err, track) {
+let getMacAddress = function () {
+    return new Promise(function (fulfill, reject) {
+        exec('hcitool con | grep SLAVE | grep -o -E "([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}"', function (err, stdout, stderr) {
             if (err) {
-                console.log(err);
+                reject(err);
                 return;
             }
-            console.log(track);
+            fulfill(stdout.replace('\n', ''));
         });
-    }, 5000);
+    }); 
+};
+
+let getBusInterface = function (macAddress) {
+    return new Promise(function (fulfill, reject) {
+        bus.getInterface('org.bluez', '/org/bluez/hci0/dev_' + macAddress.replace(/\:/g, '_') + '/player0', 'org.bluez.MediaPlayer1', (err, interface) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            fulfill(interface);
+        });
+    });
+};
+
+let setSocketListeners = function (interface) {
+    // nothing yet
+};
+
+let useDeviceForControls = function () {
+    getMacAddress()
+        .then(getBusInterface)
+        .then(setSocketListeners)
+        .catch((err) => {
+            console.log('Error while setting the device for control use', error);
+        });
+}
+
+monitor.on('add', function (device) {
+    if (device.SUBSYSTEM === 'bluetooth') {
+
+        /*
+         * For some reason hcitool does not receive the device as fast as udev does, for this reason
+         * we currently use a timeout to ensure we can retrieve the MAC address. This is totally wonky
+         * and error prone and should be fixed.
+         *
+         * TODO: find a better solution for checking the MAC address
+         */ 
+        setTimeout(useDeviceForControls, 1000);
+    }
 });
