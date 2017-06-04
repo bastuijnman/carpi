5552eaf3bc0d309d535e396e2f081cc00a04ddaa
Bas Tuijnman <bas.tuijnman@24i.com>
2017-06-04 14:43:58 +0200

    add socket server

--- src/js/ui/index.js
+++ src/js/ui/index.js
@@ -2,12 +2,14 @@ let express = require('express');
 let app = express();
 let http = require('http').Server(app);
 let path = require('path');
+let io = require('socket.io')(http);
 
-// app.get('/', (request, response) => {
-//     response.send('<h1>CarPI UI</h1>');
-// });
 app.use('/', express.static(path.join(__dirname, 'assets')));
 
-http.listen(3000, function () {
+http.listen(3000, () => {
     console.log('Server started listening');
+});
+
+io.on('connection', () => {
+    console.log('A client started listening');
 });
