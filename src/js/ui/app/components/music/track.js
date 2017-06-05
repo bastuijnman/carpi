0556a1a53c59c4a607010d6e5488945a4a30c233
Bas Tuijnman <bas.tuijnman@24i.com>
2017-06-05 14:25:42 +0200

    Updated font-weights & re-added album art

--- src/js/ui/app/components/music/track.js
+++ src/js/ui/app/components/music/track.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const containerStyle = { textAlign: 'center' };
-const h1Style = { fontFamily: 'OpenSans, sans-serif', fontSize: '2rem', fontWeight: 'lighter', color: '#FFFFFF' };
+const h1Style = { fontFamily: 'OpenSans, sans-serif', margin: '0.5rem 0', fontSize: '2rem', fontWeight: 'lighter', color: '#FFFFFF' };
 const h2Style = Object.assign({}, h1Style, { color: 'rgba(255, 255, 255, 0.5)' });
+const imgStyle = { margin: '1rem' };
 
 export default class Track extends React.Component {
 
@@ -10,6 +11,7 @@ export default class Track extends React.Component {
 
         return (
             <div style={containerStyle}>
+                <img style={imgStyle} src="img/album-art.png" />
                 <h1 style={h1Style}>{this.props.artist}</h1>
                 <h2 style={h2Style}>{this.props.title}</h2>
             </div>
